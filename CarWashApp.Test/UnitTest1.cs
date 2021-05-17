21e400cdd615092f2b0e37c23aaea3ac0c6ebe99
Deividt Gemeli <deividt@gmail.com>
2021-05-16 23:19:50 -0300

    Criado projeto de Testes

---
+++ CarWashApp.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+// ----------------------------------------------------------------------
+// <copyright file="UnitTest1.cs" company="Deividt Gemeli">
+// Copyright (c) Deividt Gemeli.
+// </copyright>
+// ----------------------------------------------------------------------
+
+namespace CarWashApp.Test
+{
+    using Xunit;
+
+    /// <summary>
+    ///  Classe <see cref="UnitTest1"/>
+    /// </summary>
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Método Test1
+        /// </summary>
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
